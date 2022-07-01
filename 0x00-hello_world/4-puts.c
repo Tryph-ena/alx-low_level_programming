a8f1a4b170d8d6c4de68ae7768efb5f176951ae5
Tryph-ena <stesha534@gmail.com>
2022-06-30 17:24:11 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ * A description should be stated
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"programming is like a multilingual puzzle");
+return (0);
+}
