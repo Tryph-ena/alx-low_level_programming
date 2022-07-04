2060b5c5d87b772fc2ff3ddedbb68ae41afa5e3d
Tryph-ena <stesha534@gmail.com>
2022-07-04 05:36:35 -0700

    print numbers

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,15 +3,17 @@
  *main - entry
  *prints alphabets with lowercase, then uppercase
  *return: 0
- */
+ */ int n;
 int main(void);
 {
 char c;
 char C;
+{
 for (c = 'a'; c <= 'z'; c++)
 putchar(c);
 for (C = 'A'; C <= 'Z'; C++)
 putchar(C);
 putchar('\n');
+}
 return (0);
 }
