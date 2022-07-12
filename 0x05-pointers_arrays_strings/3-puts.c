4be7e2edc370840447419e493f486c620182a918
Tryph-ena <stesha534@gmail.com>
2022-07-12 16:41:28 -0700

    prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,16 +7,10 @@
 void _puts(char *str)
 {
 int count = 0;
-while (count >= 0)
-{
-if (str[count] != '\0')
+while (str[count] != '\0')
 {
 _putchar(str[count]);
 count++;
-} else
-{
-count = -1;
+}
 _putchar('\n');
-}
-}
 }
